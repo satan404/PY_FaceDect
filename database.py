8f19f744d92bd6d5ef4e2659e4911891be4c4ee3
S.H <steven.huang@doublegreen.com>
2019-01-23 11:23:37 +0800

    First Commit

---
+++ database.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+conn.execute('''CREATE TABLE COMPANY
+       (ID INT PRIMARY KEY     NOT NULL,
+       NAME           TEXT    NOT NULL,
+       AGE            INT     NOT NULL
+       );''')
+
+conn.close()
